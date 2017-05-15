94de26179c3bc4fefde9f0341162afb4a78b4414
HiiYL <yonglian146@gmail.com>
2017-05-15 16:26:02 +0800

    fixed data normalization

--- train_mle_coco.py
+++ train_mle_coco.py
@@ -46,14 +46,16 @@ def run(save_path, args):
         transforms.Scale((args.image_size,args.image_size)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
         ])
 
 
     test_transform = transforms.Compose([
         transforms.Scale((args.image_size,args.image_size)),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
         ])
     
     # Load vocabulary wrapper.
